8d5b6a417ccbad7647d252be4762375f090ef58e
ViviMoya <v.moyacapitan@gmail.com>
2024-07-31 15:04:00 -0400

    se crea interaccion del usuario para darkMode

--- src/assets/images/components/Header.jsx
+++ src/assets/images/components/Header.jsx
@@ -1,19 +1,29 @@
-import IconMoon from "./icons/IconMoon"
-
+import { useEffect, useState } from "react";
+import IconMoon from "./icons/IconMoon";
+import IconSun from "./icons/IconSun";
 
 const Header = () => {
-	return (
-<header className="container mx-auto px-4 pt-8">
-                    <div className="flex justify-between">
-                        <h1 className="uppercase text-white text-2xl font-bold tracking-[0.3em]">
-                            todo
-                        </h1>
-                        <button>
-                            <IconMoon />
-                        </button>
-                    </div>
-                </header>
-	)
-}
+    const [darkMode, setDarkMode] = useState(true);
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [darkMode]);
 
-export default Header
+    return (
+        <header className="container mx-auto px-4 pt-8">
+            <div className="flex justify-between">
+                <h1 className="uppercase text-white text-2xl font-bold tracking-[0.3em]">
+                    todo
+                </h1>
+                <button onClick={() => setDarkMode(!darkMode)}>
+                    {darkMode ? <IconSun /> : <IconMoon />}
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
